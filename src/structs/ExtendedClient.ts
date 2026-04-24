52e93b38175825da717898f676f2b938c7da42ff
Vito <87043524+Heisah@users.noreply.github.com>
2026-04-24 17:45:14 -0300

    Base montada
    Todas as configurações e dependências inseridas.

---
+++ src/structs/ExtendedClient.ts
@@ -0,0 +1,19 @@
+import { BitFieldResolvable, Partials, Client, GatewayIntentsString, IntentsBitField, GuildScheduledEvent, Message } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+export class ExtendedClient extends Client {
+    constructor(){
+        super({
+            intents: Object.keys(IntentsBitField.Flags) as BitFieldResolvable<GatewayIntentsString, number>,
+            partials: [
+                Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent,
+                Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User
+            ]
+        })
+    }
+    public start(){
+        this.login(process.env.BOT_TOKEN);
+}
+}
+
